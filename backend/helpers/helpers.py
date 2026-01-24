a26b039b6408e3f665b57afa9bab6cda82627847
Shane McClain <hide579c@gmail.com>
2026-01-24 13:06:17 -0600

    removed print statements

--- backend/helpers/helpers.py
+++ backend/helpers/helpers.py
@@ -2,15 +2,12 @@ from langgraph.graph.state import CompiledStateGraph
 from IPython.display import Image
 import PIL.Image
 from io import BytesIO
-from pathlib import Path
 
 
 def save_langgraph_graph(path: str, graph: CompiledStateGraph) -> None:
     mermaid = graph.get_graph().draw_mermaid_png()
     buffer = BytesIO(Image(mermaid).data)
     img = PIL.Image.open(buffer)
-    current_dir = Path(__file__).resolve().parent.joinpath(path)
-    print(f"Path: {path}")
-    print(f"Saving graph to '{current_dir}'.")
+    print(f"Saving graph to '{path}'.")
     img.save(path)
     return None
